7fffceeaaf7259f533eaaec13a04fa7d5f1c397e
cerattobruno <cerattobruno@gmail.com>
2019-09-11 16:59:55 -0300

    Tema array resuelto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,24 @@ import { Participante } from './participante.models';
 export class AppComponent {
   title = 'sorteo-ies';
 
-  // participantes: Array<string> = ['Bruno Ceratto', 'Mauro Blacona', 'Hernan Giudici'];
-
-  public inscriptos: Array<Participante>;
+  participantes: Array<string> = ['Felicitas Centeno', 'Belen Orduna', 'Nadia Zampar',
+    'Francisco Torres', 'Fiorela Ermacora', 'Candelaria Candioti', 'Candela Rivasseau',
+    'Abril Cuello', 'Victoria De Caminos', 'Jonatan Guimenez', 'Pedro De Caminos', 'Yamina Mangisch',
+    'Nahir Ramírez', 'Micaela	Espindola', 'Lionela Cicerchia', 'Agustina Nacimbera', 'Brenda Sánchez',
+    'Belen Pautasso', 'Daiana Gauna', 'Luciana Bonahora', 'Victoria Amor', 'Lara Losano', 'Loreley Diaz',
+    'Melina Rojas', 'Agustina Landra', 'Daiana Yacob', 'Tiziana Nani', 'Brian Gabriel Ahuir',
+    'Mauro Zanella', 'Valentina Salina', 'Yael Arredondo', 'Guillermina Paulon', 'Ileana Sala',
+    'Milagros Cimadevilla', 'Josefina Giorgi', 'Sofía Neville', 'Candela Medrano',
+    'Fiore Escobar Ranzani', 'Abril Fernandez', 'Emiliano Rabbia', 'Alexis Funes', 'Luciano Bahl',
+    'Maia Espindola', 'Rosalia Nardelli', 'Milagros Gomez', 'Milena Consentino', 'Lucrecia Martinez',
+    'Julian Yappert', 'Daiana Diaz', 'Tamara Diaz'
+    ];
+    
 
   public ganadores: Array<any> = [];
+
+  /*
+  public inscriptos: Array<Participante>;
 
   public p1: Participante;
   public p2: Participante;
@@ -53,6 +66,7 @@ export class AppComponent {
     this.inscriptos = new Array (this.p1, this.p2, this.p3, this.p4, this.p5, this.p6, this.p7, this.p8,
       this.p9, this.p10, this.p11, this.p12, this.p13, this.p14, this.p15, this.p16);
   }
+  */
 
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -60,10 +74,10 @@ export class AppComponent {
 
   sorteo() {
     for ( let i = 0; i < 3; i++ ) {
-      const ganador = this.getRandomInt(1, 16);
+      const ganador = this.getRandomInt(1, 400);
       this.ganadores.push(ganador);
-      console.log(ganador);
-      console.log(this.ganadores[5]);
+      // console.log(ganador);
+      // console.log(this.ganadores[]);
     }
     // console.log( ganador );
     // console.log( this.ganadores );
